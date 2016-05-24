c86f659e09d33423707a1c71e275162d570c6a61
Jimmy Adames <jimmy.adames@gmail.com>
2016-05-24 14:41:19 -0400

    added email functionality

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
 class StaticPagesController < ApplicationController
   def index
   end
+  def thank_you
+  @f_name = params[:f_name]
+  @l_name = params[:l_name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'jimmy.adames@gmail.com',
+      :subject => "A new contact message from #{@f_name} #{@l_name}",
+      :body => @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+
+  get 'home', to: 'static_pages#index'
+
+  post 'static_pages/thank_you'
 end
